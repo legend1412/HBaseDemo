c0b61ab5f0b3a0de5db65571d82d63d00a2b544d
legend1412 <zhaojianhao_1984@163.com>
2020-03-26 18:59:41 +0800

    update

--- src/main/java/hbase/SparkHbase.scala
+++ src/main/java/hbase/SparkHbase.scala
@@ -1,5 +1,8 @@
 package hbase
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.client.{HTable, Put}
 import org.apache.hadoop.hbase.mapred.TableOutputFormat
@@ -31,13 +34,13 @@ object SparkHbase {
         .enableHiveSupport()
         .getOrCreate()
 
-      val rdd = spark.sql("select order_id,user_id,order_dow from badou.orders where order_id != 'order_id' limit 50").rdd
+      val rdd = spark.sql("select order_id,user_id,order_dow from badou.orders limit 300").rdd
       rdd.take(5).foreach(println)
-//        [2539329,1,2]
-//        [2398795,1,3]
-//        [473747,1,3]
-//        [2254736,1,4]
-//        [431534,1,4]
+      //[2539329,1,2]
+      //[2398795,1,3]
+      //[473747,1,3]
+      //[2254736,1,4]
+      //[431534,1,4]
 
       /**
        * 一个put对象就是一行记录，在构造方法中主键rowkey（user_id）
@@ -45,25 +48,24 @@ object SparkHbase {
        **/
       rdd.map { row =>
         val order_id = row(0).asInstanceOf[String]
-        //        val user_id = row(1).asInstanceOf[Long]
-        //        val order_dow = row(2).asInstanceOf[Int]
-        val user_id = row(1).asInstanceOf[String]
-        val order_dow = row(2).asInstanceOf[String]
+        val user_id = row(1).toString
+        val order_dow = row(2).toString
 
-        //      加处理逻辑user_id为主key
-        var p = new Put(Bytes.toBytes(user_id))
-        //      id 列族存放所有id类型列，order为列，value对应的order_id
+        //加处理逻辑user_id为主key
+        val p = new Put(Bytes.toBytes(user_id),tranTimeToLong(new Date().toString))
+        //id 列族存放所有id类型列，order为列，value对应的order_id
         p.addColumn(Bytes.toBytes("id"), Bytes.toBytes("order"), Bytes.toBytes(order_id))
-        //      num为列族存放所有num数值型列，dow为列，order_dow为具体值
+        //num为列族存放所有num数值型列，dow为列，order_dow为具体值
         p.addColumn(Bytes.toBytes("num"), Bytes.toBytes("dow"), Bytes.toBytes(order_dow))
         p
       }.foreachPartition { partiton =>
+        //实例化配置信息
         val jobconf = new JobConf(HBaseConfiguration.create())
         jobconf.set("hbase.zookeeper.quorum", ZOOKEEPER_QUORUM)
         jobconf.set("hbase.zookeeper.property.clientPort", "2181")
         jobconf.set("zookeeper.znode.parent", "/hbase")
         jobconf.setOutputFormat(classOf[TableOutputFormat])
-        //      写入表名
+        //写入表名
         val table = new HTable(jobconf, TableName.valueOf("badou.orders"))
         //将scala中的seq结构转变成java的list结构
         import scala.collection.JavaConversions._
@@ -73,5 +75,13 @@ object SparkHbase {
       case ex: Exception =>
         println(ex.printStackTrace())
     }
+  }
+
+  def tranTimeToLong(tm:String) :Long={
+    val fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val dt = fm.parse(tm)
+    val aa = fm.format(dt)
+    val tim: Long = dt.getTime()
+    tim
   }
 }
